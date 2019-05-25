d5adb6bc4a5857f3d71859f512d15ee0732096d3
高语越 <gaoyuyue@outlook.com>
2019-05-25 15:59:13 +0800

    should throw out of range answer exception when number is more than 10

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,22 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
-
+    @Test(expected = OutOfRangeAnswerException.class)
+    public void should_throw_out_of_range_answer_exception_when_number_is_more_than_10() throws OutOfRangeAnswerException {
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        when(randomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("2 6 9 11");
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        answerGenerator.generate();
+    }
 }
 

2825ca971f6a8c5848833e77aa9fb2ba7d48fb5b
高语越 <gaoyuyue@outlook.com>
2019-05-25 14:42:40 +0800

    should return random numbers string

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,30 @@
 package tw.core;
 
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+    private RandomIntGenerator randomIntGenerator;
+    @Before
+    public void before() {
+        randomIntGenerator = new RandomIntGenerator();
+    }
 
+    @Test
+    public void should_return_random_numbers_string() {
+        String nums = randomIntGenerator.generateNums(30, 5);
+        Assert.assertNotNull(nums);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_illegal_argument_exception_when_digitmax_less_than_numbers_of_need() {
+        randomIntGenerator.generateNums(4, 6);
+    }
 }

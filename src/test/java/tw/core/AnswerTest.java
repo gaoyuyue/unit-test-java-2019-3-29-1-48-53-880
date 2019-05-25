be640986f2ef48104aac7e2a9ba55b3c9f2377b3
高语越 <gaoyuyue@outlook.com>
2019-05-25 15:24:11 +0800

    add test should pass validate

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -2,6 +2,7 @@ package tw.core;
 
 import org.junit.Assert;
 import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
 
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
@@ -10,5 +11,11 @@ public class AnswerTest {
     @Test
     public void should_return_answer() {
         Assert.assertNotNull(Answer.createAnswer("1 2 3 4"));
+    }
+
+    @Test
+    public void should_pass_validate() throws OutOfRangeAnswerException {
+        Answer answer = Answer.createAnswer("1 2 3 4");
+        answer.validate();
     }
 }

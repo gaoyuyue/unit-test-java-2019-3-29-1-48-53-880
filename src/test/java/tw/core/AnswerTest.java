d1affa02b3325ca59be8487fcd1e322fdd42d0df
高语越 <gaoyuyue@outlook.com>
2019-05-25 15:18:46 +0800

    add test should return answer

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,14 @@
 package tw.core;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
-
+    @Test
+    public void should_return_answer() {
+        Assert.assertNotNull(Answer.createAnswer("1 2 3 4"));
+    }
 }

03774e42d15bd8852c6ff787cfc90b6af45f9f79
高语越 <gaoyuyue@outlook.com>
2019-05-25 14:12:45 +0800

    should return false when input number string count is not 4

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,18 @@
 package tw.core;
 
+import org.junit.Assert;
+import org.junit.Test;
+import tw.validator.InputValidator;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
-
+    @Test
+    public void should_return_false_when_input_number_string_count_is_not_4() {
+        InputValidator inputValidator = new InputValidator();
+        Assert.assertFalse(inputValidator.validate("1 2 3"));
+        Assert.assertFalse(inputValidator.validate("1 2 3 4 5"));
+        Assert.assertFalse(inputValidator.validate(""));
+    }
 }

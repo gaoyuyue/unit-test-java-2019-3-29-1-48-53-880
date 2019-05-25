ecfc735f135242dd0dedef633148443c4a950d59
高语越 <gaoyuyue@outlook.com>
2019-05-25 15:46:35 +0800

    should return 0A0B when no number is correct

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,22 +1,30 @@
 package tw.core;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.model.Record;
 
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    private Answer defaultAnswer;
+
+    @Before
+    public void before() {
+        defaultAnswer = Answer.createAnswer("1 2 3 4");
+    }
+
     @Test
     public void should_return_answer() {
-        Assert.assertNotNull(Answer.createAnswer("1 2 3 4"));
+        Assert.assertNotNull(defaultAnswer);
     }
 
     @Test
     public void should_pass_validate() throws OutOfRangeAnswerException {
-        Answer answer = Answer.createAnswer("1 2 3 4");
-        answer.validate();
+        defaultAnswer.validate();
     }
 
     @Test(expected = OutOfRangeAnswerException.class)
@@ -24,4 +32,13 @@ public class AnswerTest {
         Answer answer = Answer.createAnswer("1 22 3 4");
         answer.validate();
     }
+
+    @Test
+    public void should_return_0A0B_when_no_number_is_correct() {
+        Answer answer = Answer.createAnswer("7 9 5 8");
+        Record record = defaultAnswer.check(answer);
+        Assert.assertEquals("0A0B", record.getValue());
+    }
+
+
 }
